07e213dd9dca0bf5c60006394134ad21e1dc15de
Orleien <orelien.soany@gmail.com>
2021-12-06 20:36:47 +0300

    fix

--- src/components/common/filter-bar.tsx
+++ src/components/common/filter-bar.tsx
@@ -3,6 +3,7 @@ import ProductTypeMenu from "@components/layout/navbar/product-type-menu";
 import { FilterIcon } from "@components/icons/filter-icon";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+import { ShopIcon } from "@components/icons/shop-icon";
 
 export default function FilterBar() {
   const { t } = useTranslation("common");
@@ -19,7 +20,7 @@ export default function FilterBar() {
         onClick={handleSidebar}
         className="flex items-center h-8 md:h-10 py-1 md:py-1.5 px-3 md:px-4 text-sm md:text-base bg-gray-100 bg-opacity-90 rounded border border-border-200 font-semibold text-heading transition-colors duration-200 focus:outline-none hover:border-accent-hover focus:border-accent-hover hover:bg-accent focus:bg-accent hover:text-light focus:text-light"
       >
-        <FilterIcon width="18" height="14" className="me-2" />
+        <ShopIcon width="18" height="14" className="me-2" />
         {t("text-filter")}
       </button>
      {/**<ProductTypeMenu btnClassName="text-heading transition-colors duration-200 hover:text-accent focus:text-accent" /> */} 
