4007b9e19ef28346be39ee94e7433cd8b0dd5f74
Orleien <orelien.soany@gmail.com>
2022-05-02 09:40:34 +0300

    remove other local

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    locales: ["en", "de", "es", "ar", "he", "zh","fr"],
+    locales: ["en","fr"],
     defaultLocale: "fr",
     localeDetection: false,
   },
